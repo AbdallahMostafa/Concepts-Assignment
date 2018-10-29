ce01fd613618c72b818f8a5c7fc9c3e3f478587b
AbdallahMostafa <abdallahmostafa377@gmail.com>
2018-10-29 14:44:28 +0200

    The armstrong problem was modified in the function i removed i and put the number in the for loop

--- Armstrong.py
+++ Armstrong.py
@@ -1,15 +1,15 @@
 def armstrong (number):
     sum = 0
-    for j in str(i):
+    for j in str(number):
         slice = int(j) % 10
-        sum += slice ** len(str(i))
+        sum += slice ** len(str(number))
         j = int(j) / 10
     if sum == i:
         return True
     return False
 
-start = int(input())
-end = int(input())
+start = int(input("Enter the start number"))
+end = int(input("Enter the end number"))
 
 for i in range(start, end+1):
     if armstrong(i):
